87922d35a834a047b7a1d21e0b2eab5038e195b1
Alexander Erlandsson <alexander.erlandsson@icloud.com>
2018-03-13 22:12:26 +0100

    Added more core components

--- src/assets/toolkit/scripts/default.js
+++ src/assets/toolkit/scripts/default.js
@@ -1,5 +1,10 @@
 $(function() {
 
-  
+  console.log('DEBUG: Javascripts loaded');
+
+  $('[data-am-header] .main-nav-btn').on('click', function() {
+    $(this).toggleClass('is-active');
+    $('[data-am-mainnavigation]').toggleClass('is-open');
+  });
 
 });
